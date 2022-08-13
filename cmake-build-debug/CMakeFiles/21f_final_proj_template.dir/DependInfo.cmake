9d79ddf43383ca979d43b424d2dc3272ad6d3324
scott-mackenzie-19 <samackenzie@smu.edu>
2022-08-13 16:43:27 -0500

    added test files

--- cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/DependInfo.cmake
@@ -1,22 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/article.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/article.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/catch_setup.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/catch_setup.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/main.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/main.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/porter2_stemmer.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/porter2_stemmer.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/query.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/query.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/rwfile.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/rwfile.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/tests.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/tests.cpp.o"
-  "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/word.cpp" "/Users/katebouis/Desktop/cs2341/21f-srch-ngn-kate-scott/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/word.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/article.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/article.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/catch_setup.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/catch_setup.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/main.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/main.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/porter2_stemmer.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/porter2_stemmer.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/query.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/query.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/rwfile.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/rwfile.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/tests.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/tests.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/Test5/word.cpp" "/mnt/c/Users/dalla/CLionProjects/Test5/cmake-build-debug/CMakeFiles/21f_final_proj_template.dir/word.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
